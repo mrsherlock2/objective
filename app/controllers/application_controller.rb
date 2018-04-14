7e9f24692bb4432eaf10f4addc76d11863b3390d
Darshit Sharma <drshtsharma620@gmail.com>
2018-04-14 12:44:26 +0530

    pagination added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
     
     
     def homepage    
-    @all_journeys = Journey.all
+    @page = params[:page].blank? ? 1:params[:page].to_i
+    @count = params[:page].blank? ? 9:params[:count].to_i
+    @max_pages = (Journey.all.length.to_f/@count).ceil
+    @all_journeys = Journey.offset((@page-1)*@count).limit(@count)
     end
     
 end
